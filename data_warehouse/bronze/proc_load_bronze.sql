f0ef35cdc1a83bba271d33a68b295982ee04cc54
Paula Chidiac <161069332+paula-chidiac@users.noreply.github.com>
2026-02-25 17:28:39 -0300

    Create proc_load_bronze.sql

---
+++ data_warehouse/bronze/proc_load_bronze.sql
@@ -0,0 +1,148 @@
+/*
+===============================================================================
+Criação de Procedure: Carregamento da camada bronze
+===============================================================================
+
+Objetivo: Essa procedure é destinada à camada Bronze. Ela trunca as tabelas e copia dados de arquivos CSV armazenados localmente para elas. 
+
+Exemplo de uso:
+    CALL bronze.load_bronze();
+
+ATENÇÃO: lembre-se de atualizar os destinos dos arquivos antes de rodar o script.
+===============================================================================
+*/
+
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+BEGIN -- Início da procedure
+
+	DECLARE
+	    tempo_inicio_tarefa TIMESTAMP;
+	    tempo_final_tarefa TIMESTAMP;
+	    duracao INT;
+		tempo_inicio_batch TIMESTAMP;
+		tempo_final_batch TIMESTAMP;
+
+	BEGIN -- Início do "Try"
+
+	tempo_inicio_batch := NOW();
+
+		RAISE NOTICE '================================================';
+		RAISE NOTICE 'Copiando dados para a Camada Bronze';
+		RAISE NOTICE '================================================';
+
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.customers';
+		TRUNCATE TABLE bronze.customers;
+		RAISE NOTICE '>> Copiando dados para bronze.customers';
+		COPY bronze.customers
+		FROM 'D:\Projetos\Olist\olist_customers_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+    	RAISE NOTICE '>> -------------';
+		
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.sellers';
+		TRUNCATE TABLE bronze.sellers;
+		RAISE NOTICE '>> Copiando dados para bronze.sellers';
+		COPY bronze.sellers
+		FROM 'D:\Projetos\Olist\olist_sellers_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		RAISE NOTICE '>> Truncando bronze.products';
+		TRUNCATE TABLE bronze.products;
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Copiando dados para bronze.products';
+		COPY bronze.products
+		FROM 'D:\Projetos\Olist\olist_products_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.orders';
+		TRUNCATE TABLE bronze.orders;
+		RAISE NOTICE '>> Copiando dados para bronze.orders';
+		COPY bronze.orders
+		FROM 'D:\Projetos\Olist\olist_orders_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		RAISE NOTICE '>> Truncando bronze.order_items';
+		TRUNCATE TABLE bronze.order_items;
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Copiando dados para bronze.order_items';
+		COPY bronze.order_items
+		FROM 'D:\Projetos\Olist\olist_order_items_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.order_payments';
+		TRUNCATE TABLE bronze.order_payments;
+		RAISE NOTICE '>> Copiando dados para bronze.order_payments';
+		COPY bronze.order_payments
+		FROM 'D:\Projetos\Olist\olist_order_payments_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.order_reviews';
+		TRUNCATE TABLE bronze.order_reviews;
+		RAISE NOTICE '>> Copiando dados para bronze.order_reviews';
+		COPY bronze.order_reviews
+		FROM 'D:\Projetos\Olist\olist_order_reviews_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+
+		tempo_inicio_tarefa := NOW();
+		RAISE NOTICE '>> Truncando bronze.geolocation';
+		TRUNCATE TABLE bronze.geolocation;
+		RAISE NOTICE '>> Copiando dados para bronze.geolocation';
+		COPY bronze.geolocation
+		FROM 'D:\Projetos\Olist\olist_geolocation_dataset.csv'
+		DELIMITER ','
+		CSV HEADER;
+		tempo_final_tarefa := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_tarefa - tempo_inicio_tarefa))::INT;
+		RAISE NOTICE '>> Duração do carregamento da tabela: % segundos', duracao;
+		RAISE NOTICE '>> -------------';
+		tempo_final_batch := NOW();
+		duracao := EXTRACT(EPOCH FROM (tempo_final_batch - tempo_inicio_batch))::INT;
+		RAISE NOTICE '>> Duração total do carregamento da camada Bronze: % seconds', duracao;
+    RAISE NOTICE '>> -------------';
+		
+	EXCEPTION
+		WHEN OTHERS THEN
+			RAISE NOTICE 'Mensagem de erro: %', SQLERRM;
+	END;
+END;
+$$;
